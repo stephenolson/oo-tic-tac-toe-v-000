f6fdfcfd42bf201dd93733b259a8eb480d791ff1
Stephen Olson <stephenolson@gmail.com>
2018-09-15 21:11:44 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -128,16 +128,7 @@ end
     end
   end
 
-  def play
-    until over?
-      turn
-    end
-    if won?
-      puts "Congratulations " + winner + "!"
-    elsif draw? 
-      puts "Cat's Game!"
-    else
-  end
+  
   
   
   
